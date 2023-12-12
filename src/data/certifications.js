59528e1788d1e134738e84ad3ab34b58380e2286
Adam Hennings <hennings.adam@gmail.com>
2023-12-11 17:50:14 -0700

    added images to the certifications object

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,71 +1,83 @@
+import frontEndCapstoneImg from '../img/certifications/front-end-developer-capstone.jpg'
+import frontEndProfessionalCertificateImg from '../img/certifications/meta-front-end-developer.jpg'
+import advancedReactImg from '../img/certifications/advanced-react.jpg'
+import reactBasicsImg from '../img/certifications/react-basics.jpg'
+import htmlCssImg from '../img/certifications/html-and-css-in-depth.jpg'
+import introToFrontEndDevImg from '../img/certifications/intro-to-front-end-development.jpg'
+import programmingWithJavascriptImg from '../img/certifications/programming-with-javascript.jpg'
+import principlesDesignImg from '../img/certifications/principles-of-ux-ui-design.jpg'
+import versionControlImg from '../img/certifications/version-control.jpg'
+import responsiveWebsitesImg from '../img/certifications/udemy-responsive-websites-html-css.jpeg'
+
+
 const certifications = [
   {
     name: 'Front-End Developer Capstone',
     issuedDate: 'Nov 2023',
-    img: '',
+    img: frontEndCapstoneImg,
     courseUrl: 'https://www.coursera.org/learn/meta-front-end-developer-capstone',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/P7FVCZNLYEPB'
   },
   {
     name: 'Meta Front-End Developer Professional Certificate',
     issuedDate: 'Nov 2023',
-    img: '',
+    img: frontEndProfessionalCertificateImg,
     courseUrl: 'https://www.coursera.org/professional-certificates/meta-front-end-developer',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/professional-cert/2D2833V46TE9'
   },
   {
     name: 'Advanced React',
     issuedDate: 'Oct 2023',
-    img: '',
+    img: advancedReactImg,
     courseUrl: 'https://www.coursera.org/learn/advanced-react',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/J2MYGQKEE2Z8'
   },
   {
     name: 'HTML and CSS In-Depth',
     issuedDate: 'Oct 2023',
-    img: '',
+    img: htmlCssImg,
     courseUrl: 'https://www.coursera.org/learn/html-and-css-in-depth',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/FEVKEYL3BCMS'
   },
   {
     name: 'React Basics',
     issuedDate: 'Oct 2023',
-    img: '',
+    img: reactBasicsImg,
     courseUrl: 'https://www.coursera.org/learn/react-basics',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/N7W3LE3F4BWK'
   },
   {
     name: 'Introduction to Front-End Development',
     issuedDate: 'Sept 2023',
-    img: '',
+    img: introToFrontEndDevImg,
     courseUrl: 'https://www.coursera.org/learn/introduction-to-front-end-development',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/ZSHMRNMFG77B'
   },
   {
     name: 'Programming with JavaScript',
     issuedDate: 'Sept 2023',
-    img: '',
+    img: programmingWithJavascriptImg,
     courseUrl: 'https://www.coursera.org/learn/programming-with-javascript',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/SEPHXTFRPTBH'
   },
   {
     name: 'Version Control',
     issuedDate: 'Sept 2023',
-    img: '',
+    img: versionControlImg,
     courseUrl: 'https://www.coursera.org/learn/introduction-to-version-control',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/HW8RW6BBKWCM'
   },
   {
     name: 'Build Responsive Real-World Websites with HTML and CSS',
     issuedDate: 'Sept 2023',
-    img: '',
+    img: responsiveWebsitesImg,
     courseUrl: 'https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/',
     certificateUrl: 'https://www.udemy.com/certificate/UC-7534ce45-3e11-4f28-99c7-204015730e86/'
   },
   {
     name: 'Principles of UX/UI Design',
     issuedDate: 'Oct 2023',
-    img: '',
+    img: principlesDesignImg,
     courseUrl: 'https://www.coursera.org/learn/principles-of-ux-ui-design',
     certificateUrl: 'https://www.coursera.org/account/accomplishments/verify/ZBM52ZDGLU3U'
   },
